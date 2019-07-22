55447064c97f4ebd275c5e61a3840f952b48ab90
github username <github email address>
2019-07-21 20:20:55 -0400

    cleanup add folder structure

---
+++ src/containers/Beweries.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+
+class Beweries extends Component{
+
+
+  render(){
+    return(
+      <div>
+        <h1>Beweries</h1>
+      </div>
+    )
+  }
+
+}
+
+export default Beweries;
