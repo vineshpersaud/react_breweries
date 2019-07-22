d61b85b2c78a1c3dbde9a5e131317148f478a0f7
github username <github email address>
2019-07-21 21:28:08 -0400

    main show page info correct and basic readable style

--- src/components/brewerycard.js
+++ src/components/brewerycard.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './brewerycard.css'
 
 const BreweryCard = ({brewery}) => (
-  <div>
-    <p>{brewery.name}</p>
+  <div class="BreweryCard" >
+    <h3>{brewery.name}</h3>
+    <p><b>Brewery Type -</b> {brewery.brewery_type}</p>
+    <h5>Address</h5>
+      <p>{brewery.street}</p>
+      <p>{brewery.city}, {brewery.postal_code}</p>
+      <p><a href={brewery.website_url}>Website</a></p>
   </div>
 )
 
